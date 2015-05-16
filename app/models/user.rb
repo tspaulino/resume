eabcce3e3c846230164fdfb38616f006edee07b6
Tiago Paulino <tspaulino@gmail.com>
2015-05-16 15:27:36 -0300

    Setup devise and user model

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, :last_name, presence: true
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+
+  describe "#full_name" do
+    it "returns first and last name in a single string" do
+      user = User.new(first_name: "Foo", last_name: "Bar")
+      expect(user.full_name).to eq("Foo Bar")
+    end
+  end
+end
